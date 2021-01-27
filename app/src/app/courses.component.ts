73950765b0619b0b9d8dd844923c916d9dffb0bc
amirun573 <amirunaqmar@gmail.com>
2021-01-27 15:32:19 +0800

    Directive
    Use Directive to display array and use '
    "`"

--- app/src/app/courses.component.ts
+++ app/src/app/courses.component.ts
@@ -3,8 +3,22 @@ import {Component} from '@angular/core';
 //Metadata
 @Component({
     selector:'courses',//>courses>
-    template:'<h2>Courses</h2>'
+    template: `
+        <h2>{{title}}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent{
 
+    title = "List of Courses";
+
+    courses = ["course1", "course2","course3"];
+
+    getTitle(){
+        return this.title;
+    }
 }
